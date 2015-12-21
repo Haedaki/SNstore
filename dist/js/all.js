2a5b22fd2a28004f5942a5243519a06b3bea4440
Meta <meta.penca@gmail.com>
2015-12-21 16:15:27 +0100

    dist folder added

---
+++ dist/js/all.js
@@ -0,0 +1,31 @@
+angular.module('app',['ui.router']);
+angular.module('app').config(function($stateProvider, $urlRouterProvider){
+// angular.module('app').config(function($stateProvider){
+
+	$urlRouterProvider.otherwise('/error');
+	$stateProvider.state('error', {
+		url:'/error',
+		template: '<h1>Tole je napaka</h1>'
+	});
+	$stateProvider.state('homepage', {
+		url:'/',
+		template: '<h1>Homepage</h1>'
+	});
+	// $stateProvider.state('prva', {
+	// 	url:'/prva/:id',
+	// 	template: '<h1>Prva stran</h1>',
+	// 	controller: function($stateParms, $state){
+	// 		$state.go('error');
+	// 		$state.reload();
+	// 		// alert($stateParms.id);
+	// 		if($state.is('prva')){
+	// 			//do stuff
+	// 		}
+	// 	}
+	// })
+});
+angular.module('app').directive('user', function(){
+	return {
+		templateUrl:"templates/user.html"
+	};
+});
